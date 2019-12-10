ccff6027d39b9cb9182d57027095b44186862bbc
romesh-jaya <56665179+romesh-jaya@users.noreply.github.com>
2019-12-10 10:38:23 +0530

    feat: UI enhancements

--- src/app/contacts/contact-container/contact-container.component.ts
+++ src/app/contacts/contact-container/contact-container.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
+import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
+
 @Component({
   selector: 'app-contact-container',
   templateUrl: './contact-container.component.html',
   styleUrls: ['./contact-container.component.css']
 })
 export class ContactContainerComponent implements OnInit {
+  contacts: Contact[] = [];
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router, private route: ActivatedRoute, private contactService: ContactService) { }
 
   ngOnInit() {
     if (!this.route.snapshot['_routerState'].url.includes('/favorites')) {
       this.router.navigate(['/contacts']);
     }
+    this.contacts = this.contactService.getContacts();
   }
 
   onAddContact() {
     this.router.navigate(['/contact-edit']);
   }
+
+
 
 }
